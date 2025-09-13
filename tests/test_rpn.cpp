e65109bcbe1b29f2d4bf572e07f1a56354be36b3
MrEz7994 <yurahani@yandex.ru>
2025-09-13 20:45:04 +0300

    tests: add GoogleTest and discover; fix unit tests

---
+++ tests/test_rpn.cpp
@@ -0,0 +1,18 @@
+#include <gtest/gtest.h>
+#include "rpn.h"
+#include <stdexcept>
+
+TEST(RPNTest, BasicAddition) {
+    EXPECT_DOUBLE_EQ(eval_rpn("2 3 +"), 5.0);
+}
+TEST(RPNTest, WikipediaExample) {
+    EXPECT_DOUBLE_EQ(eval_rpn("15 7 1 1 + - / 3 * 2 1 1 + + -"), 5.0);
+}
+
+TEST(RPNTest, DivisionByZero) {
+    EXPECT_THROW(eval_rpn("1 0 /"), std::runtime_error);
+}
+
+TEST(RPNTest, InvalidExpression) {
+    EXPECT_THROW(eval_rpn("2 +"), std::runtime_error);
+}
